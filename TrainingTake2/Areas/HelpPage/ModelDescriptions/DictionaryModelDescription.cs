79207083e419cbcae1002282500d9b032d74ae1c
dimgr <dimgr@DESKTOP-KNP6GD0>
2017-02-13 14:22:05 +0200

    Add project files.

---
+++ TrainingTake2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TrainingTake2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TrainingTake2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrainingTake2.WebApiApplication" Language="C#" %>
