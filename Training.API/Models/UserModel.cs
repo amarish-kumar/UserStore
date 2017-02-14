d34dd755d9713db1d1a9f29aaac39d4d86419504
Dmitriy Grek <dmitriy.grek@sparkybit.com>
2017-02-14 17:46:26 +0200

    UserModel added

---
+++ Training.API/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TrainingTake2.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
