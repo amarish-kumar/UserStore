41dccc2274ddeb78d72f3c0938dcc44c5a058c19
Dmitriy Grek <dmitriy.grek@sparkybit.com>
2017-02-14 17:49:29 +0200

    project setup

--- Training.API/App_Start/RouteConfig.cs
+++ Training.API/App_Start/RouteConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web.Http;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Training.API
@@ -15,12 +14,6 @@ namespace Training.API
                 "{controller}/{action}/{id}",
                 new {controller = "Home", action = "Index", id = UrlParameter.Optional}
             );
-
-            //routes.MapHttpRoute(
-            //    "API Default",
-            //    "api/{controller}/{id}",
-            //    new {id = RouteParameter.Optional}
-            //);
         }
     }
 }
