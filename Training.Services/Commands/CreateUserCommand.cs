a3e34d25d00b5e2a92a559ac46a9acfa16aaaa44
Dmitriy Grek <dmitriy.grek@sparkybit.com>
2017-02-16 14:53:29 +0200

    Training.Services project added

---
+++ Training.Services/Commands/CreateUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.Services
+{
+    public class CreateUserCommand
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+    }
+}
