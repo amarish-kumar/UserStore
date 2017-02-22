5dee284955341c9b653276d5cf1ac76636e981d5
Dmitriy Grek <dmitriy.grek@sparkybit.com>
2017-02-22 16:37:20 +0200

    Login and registration UI files location changed

---
+++ Training.API/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using Training.DAL.Interfaces.Interfaces;
+using TrainingTake2.Services;
+
+namespace TrainingTake2.Controllers
+{
+    [EnableCors("http://localhost:3000", "*", "*")]
+    public class AccountController : ApiController
+    {
+        public AccountController(IUserRepository repository, IQueueService queue)
+        {
+        }
+    }
+}
