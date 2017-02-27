12d4342488c46f22d4ab568cbfb9d01be38ef2b3
Dmitriy Grek <dmitriy.grek@sparkybit.com>
2017-02-27 17:00:37 +0200

    Receiver.cs refactored

--- Training.UserStore/Receiver.cs
+++ Training.UserStore/Receiver.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Text;
+using System.Threading.Tasks;
 using AutoMapper;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
@@ -22,44 +23,58 @@ namespace Training.UserStore
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                //todo: move queueName to settings
                 var queueName = "hello";
                 var context = new UserContext();
                 var repository = new UserRepository(context);
                 channel.QueueDeclare(queueName, false, false, false, null);
-
                 var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += async (model, ea) =>
+
+                consumer.Received += async (sender, basicDeliverEventArgs) =>
                 {
-                    var body = ea.Body;
-                    var message = Encoding.UTF8.GetString(body);
-                    //todo: handle different commands
-                    var command = JsonConvert.DeserializeObject<Command>(message);
-                    var config = new MapperConfiguration(cfg => { cfg.CreateMap<Command, User>(); });
-                    var mapper = config.CreateMapper();
-                    var user = mapper.Map<Command, User>(command);
-
-                    switch (command.Operation)
-                    {
-                        case Operation.Create:
-                            user.CreatedDate = DateTime.Now;
-                            await repository.AddAsync(user);
-                            break;
-                        case Operation.Update:
-                            var userToEdit = repository.FindOneBy(x => x.IdentityId.Equals(user.IdentityId));
-                            user.Id = userToEdit.Id;
-                            user.UpdatedDate = DateTime.Now;
-                            await repository.EditAsync(us => us.IdentityId.Equals(user.IdentityId), user);
-                            break;
-                    }
-
+                    string message;
+                    var command = GetCommand(basicDeliverEventArgs, out message);
+                    var user = GetUser(command);
+                    await ProcessCommand(repository, command, user);
                     Console.WriteLine(" [x] Received {0}", message);
                 };
+
                 channel.BasicConsume(queueName, true, consumer);
 
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
             }
+        }
+
+        private static async Task ProcessCommand(UserRepository repository, Command command, User user)
+        {
+            switch (command.Operation)
+            {
+                case Operation.Create:
+                    user.CreatedDate = DateTime.Now;
+                    await repository.AddAsync(user);
+                    break;
+                case Operation.Update:
+                    var userToEdit = repository.FindOneBy(x => x.IdentityId.Equals(user.IdentityId));
+                    user.Id = userToEdit.Id;
+                    user.UpdatedDate = DateTime.Now;
+                    await repository.EditAsync(us => us.IdentityId.Equals(user.IdentityId), user);
+                    break;
+            }
+        }
+
+        private static User GetUser(Command command)
+        {
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<Command, User>(); });
+            var mapper = config.CreateMapper();
+            var user = mapper.Map<Command, User>(command);
+            return user;
+        }
+
+        private static Command GetCommand(BasicDeliverEventArgs ea, out string message)
+        {
+            var body = ea.Body;
+            message = Encoding.UTF8.GetString(body);
+            return JsonConvert.DeserializeObject<Command>(message);
         }
     }
 }
