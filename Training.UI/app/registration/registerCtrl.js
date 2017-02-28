8473f3ab0c817837689cb131988b74cd1144472f
Dmitriy Grek <dmitriy.grek@sparkybit.com>
2017-02-28 15:37:30 +0200

    dumb user registration disabled

--- Training.UI/app/registration/registerCtrl.js
+++ Training.UI/app/registration/registerCtrl.js
@@ -3,11 +3,11 @@ app.controller('registerCtrl', ['$scope', '$window', '$location', 'userAccount',
 function registerCtrl($scope, $window, $location, userAccount) {
     $scope.message = "";
     $scope.user = {};
-    $scope.user.firstName = "15";
-    $scope.user.surname = "w";
-    $scope.user.email = "15@15.com";
-    $scope.user.dob = "01/20/2017";
-    $scope.user.password = "15";
+    // $scope.user.firstName = "15";
+    // $scope.user.surname = "w";
+    // $scope.user.email = "15@15.com";
+    // $scope.user.dob = "01/20/2017";
+    // $scope.user.password = "15";
 
     $scope.register = function() {
         userAccount.registration.registerUser({}, $scope.user,
