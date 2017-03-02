1c75465133cab0bf088f88d87c7172d479b6cd57
Dmitriy Grek <dmitriy.grek@sparkybit.com>
2017-03-02 14:48:01 +0200

    comments removed

--- Training.API/App_Start/UnityWebApiActivator.cs
+++ Training.API/App_Start/UnityWebApiActivator.cs
@@ -12,18 +12,13 @@ namespace Training.API.App_Start
         /// <summary>Integrates Unity when the application starts.</summary>
         public static void Start() 
         {
-            // Use UnityHierarchicalDependencyResolver if you want to use a new child container for each IHttpController resolution.
-            // var resolver = new UnityHierarchicalDependencyResolver(UnityConfig.GetConfiguredContainer());
-            //var resolver = new UnityDependencyResolver(UnityConfig.GetConfiguredContainer());
-
-            //GlobalConfiguration.Configuration.DependencyResolver = resolver;
+            
         }
 
         /// <summary>Disposes the Unity container when the application is shut down.</summary>
         public static void Shutdown()
         {
-            //var container = UnityConfig.GetConfiguredContainer();
-            //container.Dispose();
+            
         }
     }
 }
